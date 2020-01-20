d4cf3a36d8bc8d3766fbaab81c8b3d7dfb4d0d20
Akim95 <m.akmalhakim95@gmail.com>
2020-01-20 12:25:05 +0800

    Updates

---
+++ precache-manifest.ad52eb7f5c06e722fa06c7048aa38c08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f709bdbf24cc3630c7b720fcb1d18ea",
+    "url": "/my-contributions/index.html"
+  },
+  {
+    "revision": "ea142eaeaae0593822d3",
+    "url": "/my-contributions/static/css/main.4dc10b35.chunk.css"
+  },
+  {
+    "revision": "7896c181968afe462a86",
+    "url": "/my-contributions/static/js/2.b675f465.chunk.js"
+  },
+  {
+    "revision": "2a106e9148d7503e3876e98fd593a09a",
+    "url": "/my-contributions/static/js/2.b675f465.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea142eaeaae0593822d3",
+    "url": "/my-contributions/static/js/main.bd8ba578.chunk.js"
+  },
+  {
+    "revision": "4a329104939b8c945af6",
+    "url": "/my-contributions/static/js/runtime-main.93d954bb.js"
+  }
+]);
